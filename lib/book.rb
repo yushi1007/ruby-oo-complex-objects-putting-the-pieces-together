465639b6cd8a042b61f553b401d38754b53930f8
Yu Shi <yushiys95@gmail.com>
2021-02-03 13:37:31 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -38,3 +38,17 @@ class Book
     end
 end
 
+#Easy and fast way:
+#class Book
+    #attr_accessor :author, :page_count, :genre
+    #attr_reader :title
+  
+    #def initialize(title)
+      #@title = title
+    #end
+  
+    #def turn_page
+      #puts "Flipping the page...wow, you read fast!"
+    #end
+  #end
+
